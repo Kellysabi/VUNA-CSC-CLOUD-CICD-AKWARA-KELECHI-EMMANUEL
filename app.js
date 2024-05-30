08271ea70d407f59b74b8f6ea07486785a50586c
kelechiemmanuel888@gmail.com <kelechiemmanuel888@gmail.com>
2024-05-30 01:11:07 +0100

    second commit is this project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! \n THIS IS AKWARA KELECHI EMMANUEL \n VUG/CSC/22/6486'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
